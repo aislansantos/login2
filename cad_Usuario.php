b67ed4dc888a9124774cc6de71d53a0b1337dbc1
Aislan Santos Aprovar <aislan_85@yahoo.com.br>
2020-08-21 14:17:13 -0300

    começando tela de login com bootstrap

---
+++ cad_Usuario.php
@@ -0,0 +1,12 @@
+<?php
+require 'menu.php';
+
+?>
+
+<div>
+
+</div>
+</div>
+</body>
+
+</html>
--- principal.php
+++ principal.php
@@ -5,24 +5,14 @@ if ($_SESSION['ativo'] != true) {
     header("Location: login.php");
 }
 
+require 'menu.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<div>
 
-<body>
-    <div class="container">
-        <div>
-            <h1>Menu</h1>
-            <a href="index.php?acao=sair">Sair</a>
-        </div>
-    </div>
+</div>
+</div>
 </body>
 
 </html>
